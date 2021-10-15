571ac3aeb267cb63bc3f45e68823b102febc67b6
Aliny <aliny.souz.r@gmail.com>
2021-10-14 22:39:09 -0400

    Update JS

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 class cliente {
     nome;
     cpf;
-    agencia;
+}
+
+class contaCorrente {
     saldo;
+    agencia;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new cliente();
-const cliente2 = new cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233344;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 66622233344;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteRicardo = new contaCorrente();
+contaCorrenteRicardo.saldo = 100;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.agencia);
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(200);
 
 console.log(cliente1, cliente2);
