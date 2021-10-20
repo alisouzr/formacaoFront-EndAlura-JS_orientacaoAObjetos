e2ac1aba19f37a97c20dd7489d5dc47936cb5992
Aliny <aliny.souz.r@gmail.com>
2021-10-19 22:07:28 -0400

    Update JS

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
-class cliente {
-    nome;
-    cpf;
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-class contaCorrente {
-    saldo;
-    agencia;
+const Cliente1 = new Cliente();
+Cliente1.nome = "Ricardo";
+Cliente1.cpf = 11122233344;
 
-    sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
+const Cliente2 = new Cliente();
+Cliente2.nome = "Alice";
+Cliente2.cpf = 66622233344;
 
-            return valor;
-        }
-    }
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.saldo = 0;
+ContaCorrenteRicardo.agencia = 1001;
 
-    depositar(valor) {
-        if (valor < 0) {
-            return;
-        }
-        this.saldo += valor;
-    }
-}
+console.log(ContaCorrenteRicardo.saldo);
+ContaCorrenteRicardo.depositar(100);
+ContaCorrenteRicardo.depositar(200);
+console.log(ContaCorrenteRicardo.saldo);
 
-const cliente1 = new cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233344;
-
-const cliente2 = new cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 66622233344;
-
-const contaCorrenteRicardo = new contaCorrente();
-contaCorrenteRicardo.saldo = 0;
-contaCorrenteRicardo.agencia = 1001;
-
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(200);
-console.log(contaCorrenteRicardo.saldo);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);;
+const valorSacado = ContaCorrenteRicardo.sacar(50);;
 
 console.log(valorSacado);
-console.log(contaCorrenteRicardo.saldo);
+console.log(ContaCorrenteRicardo.saldo);
