b903cbf43bace92cfcf99130d2aafd98171f4089
Aliny <aliny.souz.r@gmail.com>
2021-10-21 21:49:32 -0400

    Update JS

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -10,9 +10,16 @@ export class ContaCorrente {
             this._cliente = novoValor;
         }
     }
+
+    get cliente() {
+        return this._cliente;
+    }
     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
 
+    get saldo() {
+        return this._saldo;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
