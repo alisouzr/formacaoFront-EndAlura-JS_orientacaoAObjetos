07983b1ee1eddf13d37b651157244b5730144830
Aliny <aliny.souz.r@gmail.com>
2021-10-21 22:06:02 -0400

    Update JS

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
+const cliente2 = new Cliente("Alice", 88822233309);
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -22,7 +17,6 @@ conta2.agencia = 102;
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log("valor: ", valor)
-console.log(conta2);
+console.log(cliente1);
 
 
